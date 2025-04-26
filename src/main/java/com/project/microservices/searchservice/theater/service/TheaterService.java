803d44f19c7f78e78fec30cf5f944472a008bc35
DummuMounika <mounikadummu.edu2024@gmail.com>
2025-04-26 12:25:06 -0500

    Adjusted the space

--- src/main/java/com/project/microservices/searchservice/theater/service/TheaterService.java
+++ src/main/java/com/project/microservices/searchservice/theater/service/TheaterService.java
@@ -1,8 +1,8 @@
 package com.project.microservices.searchservice.theater.service;
 
 public interface TheaterService {
-	
-	//public Theater createTheater(Theater theater);
+
+//  public Theater createTheater(Theater theater);
 //	public String deleteTheater(int theaterId);
 //	public Theater updateTheater(Theater theater, int theaterId);
 //	public Theater getSingleTheater(int theaterId);
