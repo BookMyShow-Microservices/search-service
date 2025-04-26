fa6ac4883031b0449a29bcee5a92d531092d1861
DummuMounika <mounikadummu.edu2024@gmail.com>
2025-04-26 12:10:52 -0500

    Added new method for new search movie api in v2

--- src/main/java/com/project/microservices/searchservice/movie/service/MovieService.java
+++ src/main/java/com/project/microservices/searchservice/movie/service/MovieService.java
@@ -1,9 +1,7 @@
 package com.project.microservices.searchservice.movie.service;
 
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-
 import com.project.microservices.searchservice.model.SearchQueryResponse;
 import com.project.microservices.searchservice.model.SearchQueryResponse1;
 
@@ -13,11 +11,12 @@ public interface MovieService {
 	
 	public List<SearchQueryResponse1> findTheatersByMovieIdAndTheaterId(Integer movieId, Integer theaterId);
 
-	public List<String> findByMovieName(String movieName);
+	public List<String> SearchByMovieName(String movieName);
+
+	public Map<String,Integer> SearchByMovieNameV2(String movieName);
 
 	public List<String> findMoviesByCity(String cityName);
 
-	public Map<Integer, String> findMoviesByCityId(Integer cityId);
+	public Map<Integer, String> findMoviesByCityV2(Integer cityId);
 
-	
 }
