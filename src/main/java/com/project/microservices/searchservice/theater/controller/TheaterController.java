162874790ad9958cd79f0df238ed07cf41c3c262
DummuMounika <mounikadummu.edu2024@gmail.com>
2025-04-26 12:24:00 -0500

    Removed commented code

--- src/main/java/com/project/microservices/searchservice/theater/controller/TheaterController.java
+++ src/main/java/com/project/microservices/searchservice/theater/controller/TheaterController.java
@@ -4,19 +4,4 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TheaterController {
-	
-//	private TheaterService theaterService;
-//
-//	@Autowired
-//	public TheaterController(TheaterService theaterService) {
-//		super();
-//		this.theaterService = theaterService;
-//	}
-	
-	
-	//@PostMapping("api/theaters")
-//	public ResponseEntity<Theater> createTheater(@RequestBody @Valid Theater theater) {
-//		return new ResponseEntity<>(theaterService.createTheater(theater), HttpStatus.CREATED);
-//	}
-	
 }
