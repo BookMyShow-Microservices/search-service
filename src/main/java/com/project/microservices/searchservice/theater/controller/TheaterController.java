075036633958cdbdb19163580855acc2dda1df02
DummuMounika <mounikadummu.edu2024@gmail.com>
2025-04-26 19:46:16 -0500

    Removed empty files

--- src/main/java/com/project/microservices/searchservice/theater/controller/TheaterController.java
+++
@@ -1,7 +0,0 @@
-package com.project.microservices.searchservice.theater.controller;
-
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-public class TheaterController {
-}
